7fe706deaa7a4bf48bee5f25578b31750cf3ff6a
Ziyad Soobhan <zsoobhan@gmail.com>
2015-11-14 13:36:54 +0000

    Adds travis.yml and conf.travis

---
+++ www/conf/travis.py
@@ -0,0 +1,27 @@
+from conf.default import *  # noqa
+
+DEBUG = TEMPLATE_DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     'travisci',
+        'USER':     'postgres',
+        'PASSWORD': '',
+        'HOST':     'localhost',
+        'PORT':     '',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ADMINS = (
+    ('Ziyad Soobhan', 'zsoobhan@gmail.com'),
+)
+
+SECRET_KEY = 'thisisateststringandisnotusedinproduction'
+GA_TRACKING_CODE = 'XXXXX-XXXX'
