8180844d1f3e3ae37e46231415ea0ce7992354de
Ziyad Soobhan <zsoobhan@gmail.com>
2015-12-07 17:20:41 +0000

    Fixes #19
    Adds resume view
    Adds resume pdf to static files
    opens and serves

--- www/content/urls.py
+++ www/content/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.HomeView.as_view(), name="home"),
     url(r'^contact/$', views.ContactFormView.as_view(), name="contact"),
+    url(r'^resume.pdf$', views.ResumeView.as_view(), name="resume"),
 )
