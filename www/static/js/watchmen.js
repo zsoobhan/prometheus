b24fe7dc008aa14151764a37acba8a9fd50c269b
Ziyad Soobhan <zsoobhan@tucows.com>
2022-08-25 20:56:43 +0100

    Updates GA

--- www/static/js/watchmen.js
+++ www/static/js/watchmen.js
@@ -1,27 +1,27 @@
 $('#footer-js').on('click', function() {
-  ga('send', 'event', 'footer', 'click', 'source-code');
+  gtag('event', 'click', {'event_category': 'footer', 'event_label': 'source-code'});
 });
 
 $('#contact-js').on('click', function() {
-  ga('send', 'event', 'contact', 'click', $(this).data('target'));
+  gtag('event', 'click', {'event_category': 'contact', 'event_label': $(this).data('target')});
 });
 
 $('#resume-js').on('click', function() {
-  ga('send', 'event', 'page', 'click', $(this).data('target'));
+  gtag('event', 'click', {'event_category': 'page', 'event_label': $(this).data('target')});
 });
 
 $('#resume-landing-js').on('click', function() {
-  ga('send', 'event', 'page', 'click', $(this).data('target'));
+  gtag('event', 'click', {'event_category': 'page', 'event_label': $(this).data('target')});
 });
 
 $('#source-js').on('click', function() {
-  ga('send', 'event', 'menu', 'click', $(this).data('target'));
+  gtag('event', 'click', {'event_category': 'menu', 'event_label': $(this).data('target')});
 });
 
 $('.social-icon').on('click', function() {
-  ga('send', 'event', 'social', 'click', $(this).data('target'));
+  gtag('event', 'click', {'event_category': 'social', 'event_label': $(this).data('target')});
 });
 
 $('.button').on('click', function() {
-  ga('send', 'event', 'button', 'click', $(this).data('target'));
+  gtag('event', 'click', {'event_category': 'button', 'event_label': $(this).data('target')});
 });
