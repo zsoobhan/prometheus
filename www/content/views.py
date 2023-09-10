1749035149f69bc3890999c9fe337759dc030269
Ziyad Soobhan <zsoobhan@gmail.com>
2023-09-10 14:58:40 +0100

    Removes contact form

--- www/content/views.py
+++ www/content/views.py
@@ -8,28 +8,9 @@ from django.http import HttpResponse
 from django.utils.translation import ugettext as _
 from django.views import generic
 
-from . import forms
 
-
-class ContactFormView(generic.FormView):
-
-    form_class = forms.ContactForm
+class ContactFormView(generic.TemplateView):
     template_name = "content/contact.html"
-    success_url = reverse_lazy("home")
-    success_message = _(
-        "Your contact message has been saved and I will be in " "touch shortly."
-    )
-
-    def form_valid(self, form):
-        form.save()
-        messages.success(self.request, self.success_message)
-        return super(ContactFormView, self).form_valid(form)
-
-    def get_form_kwargs(self):
-        # Adds answer from session to the form kwargs for human verification
-        kwargs = super(ContactFormView, self).get_form_kwargs()
-        kwargs["answer"] = self.request.session["q_and_a"]["answer"]
-        return kwargs
 
 
 class HomeView(generic.TemplateView):
